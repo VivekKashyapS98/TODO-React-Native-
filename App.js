5de85c5b5f9dadd86710c554d676b5214d6be48b
VivekKashyapS98 <vivekyeskashyap@gmail.com>
2021-03-19 18:13:49 +0530

    TODO app completed (probably)

--- App.js
+++ App.js
@@ -1,61 +1,98 @@
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import {
   Alert,
   Button,
-  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   View,
+  TouchableOpacity,
 } from "react-native";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Todos from "./components/todos";
 
 export default function App() {
   const [text, setText] = useState("");
+  const [total, setTotal] = useState(0);
   const [todos, setTodos] = useState([]);
 
+  const deleteTodoHandler = (item) => {
+    setTodos((prev) => [
+      ...prev.filter((val) => Number(val.id) !== Number(item.id)),
+    ]);
+    setTotal(total - 1);
+  };
+
+  const setDoneHandler = (item) => {
+    setTodos((prev) => [
+      ...prev.map((val) => {
+        if (Number(val.id) === Number(item.id)) {
+          return { ...val, isDone: !val.isDone };
+        } else return val;
+      }),
+    ]);
+  };
+
+  const addTodoHandler = () => {
+    if (text.length <= 1) {
+      Alert.alert("Todo can't be empty!");
+    } else {
+      setTodos((prev) => [
+        ...prev,
+        { id: String(total + Math.random()), task: text, isDone: false },
+      ]);
+      setTotal(total + 1);
+      setText("");
+    }
+    console.log(todos);
+  };
+
   return (
-    <ScrollView
-      contentContainerStyle={{
-        paddingTop: "10%",
-        flex: 1,
-        backgroundColor: "whitesmoke",
-      }}
-    >
+    <View style={styles.main}>
+      <StatusBar style="auto" />
       <View style={styles.container}>
-        <StatusBar style="auto" />
         <Text style={styles.textHeader}>TODO-list</Text>
         <View style={styles.inputRow}>
           <TextInput
             style={styles.inputText}
             placeholder="Create a TODO"
-            onChangeText={(text) => setText(text)}
+            onChangeText={(val) => setText(val)}
+            value={text}
           />
-          <Button
-            style={styles.addButton}
-            title="Add"
-            onPress={() => Alert.alert("Add button pressed!")}
-          />
+          <TouchableOpacity style={styles.addButton} onPress={addTodoHandler}>
+            <FontAwesomeIcon color="white" icon={faPlus} />
+          </TouchableOpacity>
         </View>
       </View>
-    </ScrollView>
+      <Todos
+        data={todos}
+        setDone={setDoneHandler}
+        setDelete={deleteTodoHandler}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  main: {
+    marginTop: 20,
+  },
   container: {
     height: "15%",
+    height: 100,
   },
   textHeader: {
+    textAlign: "center",
     fontSize: 25,
     fontFamily: "sans-serif",
     fontWeight: "bold",
   },
   inputText: {
     backgroundColor: "white",
-    width: 280,
-    height: 50,
-    padding: 10,
+    width: "80%",
+    padding: 5,
     margin: 10,
     borderColor: "black",
     borderWidth: 1,
@@ -67,6 +104,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   addButton: {
-    backgroundColor: "grey",
+    padding: 10,
+    backgroundColor: "#0e9aeb",
+    borderColor: "transparent",
+    borderWidth: 1,
+    borderRadius: 10,
   },
 });
--- components/todos.js
+++ components/todos.js
@@ -1,5 +1,63 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  TouchableOpacity,
+  View,
+  FlatList,
+  Text,
+  StyleSheet,
+} from "react-native";
 
-export default function Todos() {
-    return ();
+const Item = ({ item, onPress, style, setDelete }) => (
+  <TouchableOpacity onPress={onPress} style={style}>
+    <Text>{item.task}</Text>
+    <TouchableOpacity onPress={setDelete}>
+      <FontAwesomeIcon icon={faTrash} />
+    </TouchableOpacity>
+  </TouchableOpacity>
+);
+
+export default function Todos(props) {
+  const renderItem = ({ item }) => {
+    const border = item.isDone ? "#52fc03" : "#23045e";
+
+    return (
+      <Item
+        item={item}
+        onPress={() => props.setDone(item)}
+        setDelete={() => props.setDelete(item)}
+        style={{
+          ...styles.todos,
+          borderColor: border,
+          borderWidth: 1,
+          borderRadius: 5,
+        }}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={{ flex: 0, marginBottom: 10 }}
+        keyExtractor={(item) => item.id}
+        data={props.data}
+        renderItem={renderItem}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  todos: {
+    marginVertical: 5,
+    marginHorizontal: 10,
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  container: {
+    marginTop: 20,
+  },
+});
